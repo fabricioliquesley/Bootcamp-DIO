b27d7cbf87a97acb1ed0e718cca5997f50be4984
Fabricio <fabricioliquesley15@gmail.com>
2023-12-08 20:17:59 -0300

    feat: created state.js to store visualization and control states

---
+++ jogos/detona-Ralph/src/scripts/state.js
@@ -0,0 +1,12 @@
+export const state = {
+    view: {
+        squares: document.querySelectorAll(".square"),
+        enemy: document.querySelector(".enemy"),
+        score: document.querySelector("#score"),
+        timeLeft: document.querySelector("#time"),
+        lives: document.querySelector("#lives"),
+    },
+    values: {
+
+    }
+}
