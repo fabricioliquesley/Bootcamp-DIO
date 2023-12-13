170af348016b4c71127541652a7c8f6518d10fd5
Fabricio <fabricioliquesley15@gmail.com>
2023-12-12 21:16:13 -0300

    feat: countDownToStar values and views added to state.js

--- jogos/detona-Ralph/src/scripts/state.js
+++ jogos/detona-Ralph/src/scripts/state.js
@@ -5,16 +5,19 @@ export const state = {
         score: document.querySelector("#score"),
         timeLeft: document.querySelector("#time"),
         lives: document.querySelector("#lives"),
+        countDownContainer: document.querySelector('.countDown')
     },
     values: {
         gameVelocity: 1000,
         hitPosition: 0,
         score: 0,
         lives: 3,
-        currentTime: 60
+        currentTime: 60,
+        durationCountDow: 3
     },
     actions: {
         timerId: null,
         countDownTimerId: null,
+        countDownToStartId: null,
     }
 }
