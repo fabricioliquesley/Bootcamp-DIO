8cdd300082e9d7b7b91be5978a18b49215b5cf59
Fabricio <fabricioliquesley15@gmail.com>
2023-12-14 19:16:13 -0300

    feat: added logic to rotate the cards after the click

--- jogos/jogo-memoria-emojis/src/scripts/engine.js
+++ jogos/jogo-memoria-emojis/src/scripts/engine.js
@@ -9,8 +9,24 @@ let shuffleEmojis = emotes.sort(() => (Math.random() > 0.5) ? 2 : -1);
 shuffleEmojis.forEach((emote) => {
     let card = document.createElement('div');
 
-    card.classList.add(['card'],['flex']);
+    card.classList.add(['card'], ['flex']);
     card.innerHTML = emote;
+    card.onclick = handleClick;
 
     cardsContainer.append(card);
 })
+
+function handleClick() {
+    if (openCards.length < 2) {
+        this.classList.add("open");
+        openCards.push(this);
+    }
+
+    if (openCards.length == 2) {
+        setTimeout(checkMatch, 500);
+    }
+}
+
+function checkMatch() {
+    
+}
