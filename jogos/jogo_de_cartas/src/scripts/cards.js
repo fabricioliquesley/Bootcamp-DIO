8d478cd52f375598249e820d4e3ab0da0de3d951
Fabricio <fabricioliquesley15@gmail.com>
2023-12-21 12:29:08 -0300

    feat: created cardsData to define the attributes of each card

---
+++ jogos/jogo_de_cartas/src/scripts/cards.js
@@ -0,0 +1,28 @@
+const pathImages = ".src/assets/icons/";
+
+export const cardData = [
+    {
+        id: 0,
+        name: "Blue Eyes White Dragon",
+        type: "Paper",
+        img: `${pathImages}dragon.png`,
+        WinOf: [1],
+        LoseOf: [2]
+    },
+    {
+        id: 1,
+        name: "Dark magician",
+        type: "Rock",
+        img: `${pathImages}magician.png`,
+        WinOf: [2],
+        LoseOf: [0]
+    },
+    {
+        id: 2,
+        name: "Exodia",
+        type: "Scissors",
+        img: `${pathImages}exodia.png`,
+        WinOf: [0],
+        LoseOf: [1]
+    }
+];
