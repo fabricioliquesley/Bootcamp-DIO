338136705ccfddf116f4bd13804c4dcbe4c6ff20
Fabricio <fabricioliquesley15@gmail.com>
2023-12-21 12:44:35 -0300

    feat: created the function to obtain the random identification of the card

--- jogos/jogo_de_cartas/src/scripts/engine.js
+++ jogos/jogo_de_cartas/src/scripts/engine.js
@@ -1,7 +1,29 @@
 import { state } from "./state.js";
+import { cardData } from "./cards.js";
+
+const playersSide = {
+    player: "player-field-card",
+    enemy: "enemy-field-card"
+}
+
+async function getRandomCardId() {
+    const randomIndex = Math.floor(Math.random() * cardData.length);
+
+    return cardData[randomIndex].id;
+}
+
+async function drawCards(cardsNumber, fieldSide) {
+    for (let i = 0; i < cardsNumber; i++) {
+        const randomIdCard = await getRandomCardId();
+        const cardImage = await createCardImage(randomIdCard, fieldSide);
+
+        document.getElementById(fieldSide).appendChild(cardImage);
+    }
+}
 
 function init() {
-
+    drawCards(5, playersSide.player);
+    drawCards(5, playersSide.enemy);
 }
 
 init();
