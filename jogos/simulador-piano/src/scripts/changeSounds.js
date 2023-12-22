bdd858bf0b26d2443ba9e77a38ac09201bbfd829
Fabricio <fabricioliquesley15@gmail.com>
2023-12-22 00:30:43 -0300

    feat: transformed the logic to leave only the selected button active into a function

--- jogos/simulador-piano/src/scripts/changeSounds.js
+++ jogos/simulador-piano/src/scripts/changeSounds.js
@@ -6,6 +6,20 @@ function changeSound(soundFolder) {
     state.folderSoundName = soundFolder;
 }
 
+function removeAddClassActive(btn) {
+    btn.classList.add("active");
+
+    for (let i = 0; i < instrumentsBtns.length; i++) {
+        if (instrumentsBtns[i] !== btn) {
+            instrumentsBtns[i].classList.remove("active");
+        }
+    }
+}
+
 instrumentsBtns.forEach(btn => {
-    btn.addEventListener("click", () => changeSound(btn.dataset.sound));
+    btn.addEventListener("click", () => {
+        changeSound(btn.dataset.sound)
+
+        removeAddClassActive(btn);
+    });
 })
